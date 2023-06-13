b0d8baf8d4993ab001a462280a18c2f82acc2e64
JorgeMioska <quispechilojorge@gmail.com>
2023-06-13 06:07:22 -0500

    dinamic private span display

---
+++ scripts/index.js
@@ -0,0 +1,19 @@
+//Log in/sign up page functions
+
+const signUpButtons = document.querySelectorAll(".sign-up-button");
+const signUpContainer = document.querySelector("#sign-up-container");
+
+signUpButtons.forEach((el) => {
+  el.addEventListener("click", () => {
+    signUpContainer.classList.toggle("open");
+  });
+});
+
+//MainPage functions
+
+const select = document.querySelector("#select");
+
+select.addEventListener("change", () => {
+  const privateConfig = document.querySelector("#post-privacy");
+  privateConfig.innerText = select.value;
+});
--- scripts/mainPage.js
+++
@@ -1,8 +0,0 @@
-const signUpButtons = document.querySelectorAll(".sign-up-button");
-const signUpContainer = document.querySelector("#sign-up-container");
-
-signUpButtons.forEach((el) => {
-  el.addEventListener("click", () => {
-    signUpContainer.classList.toggle("open");
-  });
-});
