5b37ba591c83756bebafd9f62efe6e8944dbb0e4
JorgeMioska <quispechilojorge@gmail.com>
2023-06-30 04:44:21 -0500

    container left responsive

--- scripts/mainPage.js
+++ scripts/mainPage.js
@@ -1,7 +1,15 @@
 const select = document.querySelector("#select");
 
-
 select.addEventListener("change", () => {
   const privateConfig = document.querySelector("#post-privacy");
   privateConfig.innerText = select.value;
+});
+
+const menuRight = document.querySelector("#menu-right");
+const hamburgerButtonRight = document.querySelector(
+  "#hamburger-btn__menu-right"
+);
+
+hamburgerButtonRight.addEventListener("click", () => {
+  menuRight.classList.toggle("show-menu-left");
 });
