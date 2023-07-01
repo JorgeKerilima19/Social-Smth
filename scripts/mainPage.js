3b1b3c950fcb3b511f8f676d9fc80997869aed68
JorgeMioska <quispechilojorge@gmail.com>
2023-07-01 03:17:20 -0500

    User friend list

--- scripts/mainPage.js
+++ scripts/mainPage.js
@@ -1,12 +1,16 @@
 import displayFriend from "./components/friend.js";
 import { getUsers } from "./functions/fetchUsers.js";
 
+// Post creating privacy
+
 const select = document.querySelector("#select");
 
 select.addEventListener("change", () => {
   const privateConfig = document.querySelector("#post-privacy");
   privateConfig.innerText = select.value;
 });
+
+//responsive right menu
 
 const menuRight = document.querySelector("#menu-right");
 const hamburgerButtonRight = document.querySelector(
@@ -17,22 +21,24 @@ hamburgerButtonRight.addEventListener("click", () => {
   menuRight.classList.toggle("show-menu-left");
 });
 
+//Display User Friends active and non-active
+
 const onlineFriendsContainer = document.getElementById("active-friends");
 const offlineFriendsContainer = document.getElementById("offline-friends");
 
 getUsers().then((users) => {
-  const friendList = document.createDocumentFragment();
+  const onlineFriendList = document.createDocumentFragment();
+  const offlineFriendList = document.createDocumentFragment();
   users.forEach((el) => {
     const friend = displayFriend(el.name);
-    friendList.appendChild(friend);
+    if (el.id % 2 === 0) {
+      onlineFriendList.appendChild(friend);
+    } else {
+      offlineFriendList.appendChild(friend);
+    }
   });
-  onlineFriendsContainer.appendChild(friendList);
-});
-getUsers().then((users) => {
-  const friendList = document.createDocumentFragment();
-  users.forEach((el) => {
-    const friend = displayFriend(el.name);
-    friendList.appendChild(friend);
-  });
-  offlineFriendsContainer.appendChild(friendList);
+  offlineFriendsContainer.appendChild(offlineFriendList);
+  onlineFriendsContainer.appendChild(onlineFriendList);
+  console.log(offlineFriendList);
+  console.log(onlineFriendList);
 });
