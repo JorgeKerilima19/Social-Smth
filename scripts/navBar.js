c4f7d539663678cfe9ec53ddaa00cccb3b30f53d
JorgeMioska <quispechilojorge@gmail.com>
2023-07-03 05:47:32 -0500

    dark -Mode button

--- scripts/navBar.js
+++ scripts/navBar.js
@@ -1,14 +1,26 @@
-const userImage=document.querySelector("#profile-img");
-const showMenu=document.querySelector("#hidden-menu");
+//Show user menu
 
+const userImage = document.querySelector("#profile-img");
+const showMenu = document.querySelector("#hidden-menu");
 
-userImage.addEventListener("click",()=>{
-    showMenu.classList.toggle("show");
+userImage.addEventListener("click", () => {
+  showMenu.classList.toggle("show");
 });
 
-const menuResponsiveButton=document.getElementById("button-menu-responsive");
-const menuResponsive=document.querySelector(".header__nav__navbar");
+//Display hamburger menu on small screens
 
-menuResponsiveButton.addEventListener("click",()=>{
-    menuResponsive.classList.toggle("show-navbar")
+const menuResponsiveButton = document.getElementById("button-menu-responsive");
+const menuResponsive = document.querySelector(".header__nav__navbar");
+
+menuResponsiveButton.addEventListener("click", () => {
+  menuResponsive.classList.toggle("show-navbar");
+});
+
+//handle dark mode
+
+const darkModeContainer = document.getElementById("dark-mode");
+const darkModeButton = document.getElementById("dark-mode__button");
+
+darkModeContainer.addEventListener("click", () => {
+  darkModeButton.classList.toggle("on");
 });
