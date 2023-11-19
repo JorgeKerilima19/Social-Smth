5e367bc6b80e15df370d44f65cfec7ae52222084
JorgeMioska <quispechilojorge@gmail.com>
2023-11-19 05:30:18 -0500

    chat section bottom

--- scripts/components/chatComponents/chatSectionMessages.js
+++ scripts/components/chatComponents/chatSectionMessages.js
@@ -1,7 +1,7 @@
 const icons = [
-  { name: "audio", path: "" },
-  { name: "photo", path: "" },
-  { name: "gif", path: "" },
+  { name: "audio", path: "../../assets/svg/chatSection/audio.svg" },
+  { name: "photo", path: "../../assets/svg/chatSection/photo.svg" },
+  { name: "gif", path: "../../assets/svg/chatSection/gif.svg" },
 ];
 
 const chatSectionMessages = () => {
@@ -11,20 +11,58 @@ const chatSectionMessages = () => {
   //form
 
   const form = document.createElement("form");
-  const inputAudio = document.createElement("input");
-  const inputPhoto = document.createElement("input");
-  const inputGif = document.createElement("input");
-  const inputText = document.createElement("input");
 
-  inputAudio.type = "checkbox";
-  inputAudio.type = "file";
+  //chat options icon
+  const iconsWrapper = document.createElement("div");
 
-  section.innerText = "xdxd";
+  iconsWrapper.classList.add(
+    "flex",
+    "flex__gap-sm",
+    "chat-section__icons-wrapper"
+  );
 
-  form.appendChild(inputAudio);
-  form.appendChild(inputPhoto);
-  form.appendChild(inputGif);
-  form.appendChild(inputText);
+  icons.forEach((icon) => {
+    const optionWrapper = document.createElement("label");
+    const optionIcon = document.createElement("input");
+
+    optionWrapper.style.backgroundImage = `url(${icon.path})`;
+    optionWrapper.classList.add("icon-container__bg");
+
+    optionIcon.type = "file";
+
+    optionIcon.id = icon.name;
+    optionWrapper.htmlFor = icon.name;
+
+    optionIcon.classList.add("chat-section__input");
+
+    optionWrapper.appendChild(optionIcon);
+
+    iconsWrapper.appendChild(optionWrapper);
+  });
+
+  //text area
+
+  const labelText = document.createElement("label");
+  const inputText = document.createElement("textarea");
+
+  labelText.appendChild(inputText);
+
+  inputText.classList.add("width__full", "chat-section__input-text");
+  labelText.classList.add(
+    "width__90",
+    "margin__all-right",
+    "flex",
+    "flex__item-center"
+  );
+  form.classList.add(
+    "flex",
+    "flex__gap-sm",
+    "flex__item-center",
+    "chat-section__form"
+  );
+
+  form.appendChild(iconsWrapper);
+  form.appendChild(labelText);
 
   section.appendChild(textArea);
   section.appendChild(form);
