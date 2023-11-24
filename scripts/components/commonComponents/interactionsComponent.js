789489536209c64b9ec7d8fc2aa013f097113193
JorgeMioska <quispechilojorge@gmail.com>
2023-11-24 01:42:55 -0500

    post interactions/ reactions

---
+++ scripts/components/commonComponents/interactionsComponent.js
@@ -0,0 +1,135 @@
+import { randomNumber, randomNumber5 } from "../../functions/randomNumber.js";
+
+import commentSection from "../commentSection.js";
+
+function handleClick(postId) {
+  const commentsContainer = postId
+    ? commentSection(postId, commentsNumber)
+    : commentSection((postId = 1), commentsNumber);
+  mainContainer.appendChild(commentsContainer);
+
+  container.removeEventListener("click", handleClick);
+}
+
+const interactions = [
+  {
+    name: "Like",
+
+    svgPath: "../../assets/svg/like.svg",
+  },
+  {
+    name: "Comment",
+
+    svgPath: "../../assets/svg/comment.svg",
+  },
+  {
+    name: "Share",
+
+    svgPath: "../../assets/svg/share.svg",
+  },
+];
+
+export default function interactionsComponent(postId) {
+  //reactions
+
+  const mainContainer = document.createElement("article");
+
+  //Post Reactions
+
+  const reactions = ["Reaction", "Comment", "Share"];
+
+  let commentsNumber;
+
+  const reactionContainer = document.createElement("div");
+
+  reactionContainer.classList.add(
+    "flex",
+    "flex__sp-btw",
+    "flex__item-center",
+    "pd-sm",
+    "width__full",
+    "separator"
+  );
+
+  reactions.forEach((el) => {
+    const container = document.createElement("div");
+    const reactionNumber = document.createElement("span");
+    const reactionDescription = document.createElement("span");
+
+    container.classList.add("flex", "flex__gap-sm", "flex__item-center");
+
+    let number;
+
+    if (el === "Comment") {
+      number = randomNumber5();
+      commentsNumber = number;
+    } else {
+      number = randomNumber();
+    }
+
+    reactionNumber.innerText = number;
+    number < 2 && number > 0
+      ? (reactionDescription.innerText = el)
+      : (reactionDescription.innerText = el + "s");
+
+    container.appendChild(reactionNumber);
+    container.appendChild(reactionDescription);
+    reactionContainer.appendChild(container);
+  });
+
+  mainContainer.appendChild(reactionContainer);
+  console.log(reactionContainer);
+
+  //Interactions section
+
+  const postInteractions = document.createElement("section");
+
+  interactions.forEach((el, index) => {
+    const container = document.createElement("div");
+    const imgSvg = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "svg"
+    );
+    const useTag = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "use"
+    );
+
+    useTag.setAttribute(
+      "href",
+      `${el.svgPath + "#interaction-" + (index + 1)}`
+    );
+
+    imgSvg.appendChild(useTag);
+
+    imgSvg.classList.add("svg-container__small", "interaction");
+
+    const interactionDescription = document.createElement("span");
+
+    interactionDescription.innerText = el.name;
+    container.classList.add(
+      "flex",
+      "flex__gap-sm",
+      "flex__item-center",
+      "to-hover",
+      "pd-sm",
+      "border__radius-sm",
+      "cursor__pointer"
+    );
+
+    if (el.name === "Comment") {
+      container.classList.add("comment-button");
+      container.addEventListener("click", () => handleClick(postId));
+    }
+
+    container.appendChild(imgSvg);
+    container.appendChild(interactionDescription);
+    postInteractions.appendChild(container);
+  });
+
+  postInteractions.classList.add("flex", "flex__sp-ard", "width__full");
+
+  mainContainer.appendChild(postInteractions);
+
+  return mainContainer;
+}
--- scripts/components/post.js
+++ scripts/components/post.js
@@ -1,8 +1,6 @@
-import { randomNumber, randomNumber5 } from "../functions/randomNumber.js";
-
 import postContainer from "./commonComponents/postContainer.js";
 
-import commentSection from "./commentSection.js";
+import interactionsComponent from "./commonComponents/interactionsComponent.js";
 
 export default function createPost(
   postId = 0,
@@ -91,124 +89,19 @@ export default function createPost(
   postMainContent.appendChild(postMainContentDescription);
   postImage ? postMainContent.appendChild(postMainContentImg) : "";
 
-  const postReactions = document.createElement("section");
-
-  postReactions.classList.add(
-    "flex",
-    "width__full",
-    "flex__sp-btw",
-    "pd-sm",
-    "separator"
-  );
-
-  //Post Reactions
-
-  const reactions = ["Reaction", "Comment", "Share"];
-
-  let commentsNumber;
-
-  const reactionContainer = document.createElement("div");
-  reactionContainer.classList.add("flex", "flex__gap-sm", "flex__item-center");
-  reactions.forEach((el) => {
-    const container = reactionContainer.cloneNode(true);
-    const reactionNumber = document.createElement("span");
-    const reactionDescription = document.createElement("span");
-
-    let number;
-
-    if (el === "Comment") {
-      number = randomNumber5();
-      commentsNumber = number;
-    } else {
-      number = randomNumber();
-    }
-
-    reactionNumber.innerText = number;
-    number < 2 && number > 0
-      ? (reactionDescription.innerText = el)
-      : (reactionDescription.innerText = el + "s");
-
-    container.appendChild(reactionNumber);
-    container.appendChild(reactionDescription);
-    postReactions.appendChild(container);
-  });
-
   //Post Interactions
 
-  const postInteractions = document.createElement("section");
+  const postInteractionData = interactionsComponent(postId);
 
-  const interactions = [
-    {
-      name: "Like",
-
-      svgPath: "../../assets/svg/like.svg",
-    },
-    {
-      name: "Comment",
-
-      svgPath: "../../assets/svg/comment.svg",
-    },
-    {
-      name: "Share",
-
-      svgPath: "../../assets/svg/share.svg",
-    },
-  ];
-
-  interactions.forEach((el, index) => {
-    const container = reactionContainer.cloneNode(true);
-    const imgSvg = document.createElementNS(
-      "http://www.w3.org/2000/svg",
-      "svg"
-    );
-    const useTag = document.createElementNS(
-      "http://www.w3.org/2000/svg",
-      "use"
-    );
-
-    useTag.setAttribute(
-      "href",
-      `${el.svgPath + "#interaction-" + (index + 1)}`
-    );
-
-    imgSvg.appendChild(useTag);
-
-    imgSvg.classList.add("svg-container__small","interaction");
-
-    const interactionDescription = document.createElement("span");
-
-    interactionDescription.innerText = el.name;
-    container.classList.add(
-      "to-hover",
-      "pd-sm",
-      "border__radius-sm",
-      "cursor__pointer"
-    );
-
-    function handleClick() {
-      const commentsContainer = postId
-        ? commentSection(postId, commentsNumber)
-        : commentSection((postId = 1), commentsNumber);
-      mainPostContainer.appendChild(commentsContainer);
-
-      container.removeEventListener("click", handleClick);
-    }
-
-    if (el.name === "Comment") {
-      container.classList.add("comment-button");
-      container.addEventListener("click", handleClick);
-    }
-
-    container.appendChild(imgSvg);
-    container.appendChild(interactionDescription);
-    postInteractions.appendChild(container);
-  });
-
-  postInteractions.classList.add("flex", "flex__sp-ard", "width__full");
+  postInteractionData.classList.add(
+    "flex",
+    "flex__column",
+    "flex__gap-md",
+    "width__full"
+  );
 
   postContentContainer.appendChild(postMainContent);
-  postContentContainer.appendChild(postReactions);
-  postContentContainer.appendChild(postInteractions);
+  postContentContainer.appendChild(postInteractionData);
 
   mainPostContainer.appendChild(authorInfo);
   mainPostContainer.appendChild(postContentContainer);
