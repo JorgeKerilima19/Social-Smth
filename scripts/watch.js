0360b24aca1e44c72c1856d2d0607e58bad938b1
JorgeMioska <quispechilojorge@gmail.com>
2023-12-07 01:46:59 -0500

    videos component/wrapper responsive

--- scripts/watch.js
+++ scripts/watch.js
@@ -6,7 +6,7 @@ import { randomNumber10 } from "./functions/randomNumber.js";
 const userId = randomNumber10() + 1;
 
 getUser(userId).then((user) => {
-  const videosContainer = document.getElementById("videos-container");
+  const videosContainer = document.getElementById("main-videos-container");
 
   const video = videoComponent(user);
   const video1 = videoComponent(user);
