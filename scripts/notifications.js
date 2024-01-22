3c614cf9c821b8b4aa7e2acfa66959557c45bd7a
JorgeMioska <quispechilojorge@gmail.com>
2024-01-22 17:23:33 -0500

    notifications + more paths corrected

---
+++ scripts/notifications.js
@@ -0,0 +1,12 @@
+const mainContainer = document.getElementById("notifications-container");
+
+mainContainer.style.fontSize = "1.5rem";
+
+mainContainer.classList.add(
+  "width__full",
+  "flex",
+  "flex__item-center",
+  "flex__sp-center"
+);
+
+mainContainer.innerText = "No new notifications";
